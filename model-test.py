0a60ffd017b36cfc114900a8f2ba46c9d19a5bcc
Binuka-Rajapaksha <binuka.shan.r@gmail.com>
2023-12-11 05:57:37 +0530

    First Commit

---
+++ model-test.py
@@ -0,0 +1,51 @@
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.applications.vgg19 import preprocess_input, decode_predictions
+import numpy as np
+
+# Load the trained model
+model = load_model("tourist_places_model_02.keras")
+
+# Function to preprocess an image for prediction
+def preprocess_image(image_path):
+    img = image.load_img(image_path, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = preprocess_input(img_array)
+    return img_array
+
+# Function to make predictions
+def predict_class(image_path):
+    img_array = preprocess_image(image_path)
+    predictions = model.predict(img_array)
+    # Get the class with the highest probability
+    predicted_class = np.argmax(predictions, axis=1)
+    return predicted_class[0]
+
+
+# Load and preprocess a new image for testing
+
+# img_path = "ZAM_1577.JPG"
+
+img_path = 'UMS Dataset/Chancellor Hall/ZAM_1400.JPG'
+# img_path = 'UMS Dataset/Chancellor Tower/ZAM_1883.JPG'
+# img_path = 'UMS Dataset/Clock Tower/ZAM_1244.JPG'
+# img_path = 'UMS Dataset/Colorfull Stairway/ZAM_1346.JPG'
+# img_path = 'UMS Dataset/DKP Baru/ZAM_1980.JPG'
+# img_path = 'UMS Dataset/Library/ZAM_1559.JPG'
+# img_path = 'UMS Dataset/Recital Hall/ZAM_1660.JPG'
+# img_path = 'UMS Dataset/UMS Aquarium/ZAM_1784.JPG'
+# img_path = 'UMS Dataset/UMS Mosque/ZAM_2102.JPG'
+
+
+predicted_class = predict_class(img_path)
+
+# Map the predicted class index to the class label
+class_labels = [
+    "Chancellor Hall", "Chancellor Tower", "Clock Tower",
+    "Colorful Stairway", "DKP Baru", "Library",
+    "Recital Hall", "UMS Aquarium", "UMS Mosque"
+]
+
+predicted_label = class_labels[predicted_class]
+print(f"The predicted class is: {predicted_label}")
---
+++ model-train.py
@@ -0,0 +1,65 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.inception_v3 import InceptionV3
+from keras.layers import GlobalAveragePooling2D, Dense
+from keras.models import Model
+from keras.optimizers import Adam
+
+# Data augmentation for training set
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    validation_split=0.2  # 80% for training, 20% for validation
+)
+
+# Load the dataset
+dataset_path = 'UMS Dataset'
+
+# training set
+train_generator = train_datagen.flow_from_directory(
+    dataset_path,
+    target_size=(299, 299),
+    batch_size=16,
+    class_mode='categorical',
+    subset='training'  # Specify the subset as 'training' for the training set
+)
+
+# Validation set
+validation_generator = train_datagen.flow_from_directory(
+    dataset_path,
+    target_size=(299, 299),
+    batch_size=16,
+    class_mode='categorical',
+    subset='validation'  # Specify the subset as 'validation' for the validation set
+)
+
+
+# Base InceptionV3 model
+base_model = InceptionV3(weights='imagenet', include_top=False)
+
+# Additional layers
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(1024, activation='relu')(x)
+
+# Add a fully connected layer with softmax activation
+predictions = Dense(9, activation='softmax')(x)
+
+# Create the final model
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# Freeze the layers of the base model
+for layer in base_model.layers:
+    layer.trainable = False
+
+# Compile the model
+model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train the Model
+model.fit(train_generator, epochs=10, steps_per_epoch=len(train_generator), validation_data=validation_generator,
+          validation_steps=len(validation_generator))
+
+
+# Save the Model
+model.save('tourist_places_model_02.keras')
